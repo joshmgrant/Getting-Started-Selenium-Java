6d3bbd6054513afe6edbd9408d16c04a83ace296
joshgrant <joshua.m.grant@gmail.com>
2019-09-25 16:03:11 -0400

    bare bones starting point

--- src/test/java/com/saucelabs/tests/WorkflowTest.java
+++ src/test/java/com/saucelabs/tests/WorkflowTest.java
@@ -19,30 +19,7 @@ public class WorkflowTest {
     @Test
     public void workflowTest() {
 
-//        DesiredCapabilities caps = DesiredCapabilities.chrome();
-//        caps.setCapability("platform", "Windows 10");
-//        caps.setCapability("version", "65.0");
-//        caps.setCapability("seleniumVersion", "3.1.0");
-//
-//        String sauce_username = System.getenv("SAUCE_USERNAME");
-//        String sauce_access_key = System.getenv("SAUCE_ACCESS_KEY");
-//
-//        URL url = null;
-//        try {
-//            url = new URL("https://" + sauce_username + ":" + sauce_access_key + "@ondemand.saucelabs.com/wd/hub/");
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        }
-
         driver = new ChromeDriver();
-
-//        driver.get("https://www.saucedemo.com");
-//
-//        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-//        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-//        driver.findElement(By.cssSelector(".btn_action")).click();
-//
-//        Assert.assertTrue(driver.getCurrentUrl().contains("/inventory.html"));
 
         driver.quit();
     }
