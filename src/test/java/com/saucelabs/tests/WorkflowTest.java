5b8a46a437e4e75f8b6253c8895649deef975676
joshgrant <joshua.m.grant@gmail.com>
2019-09-25 14:38:04 -0400

    basic starting point

--- src/test/java/com/saucelabs/tests/WorkflowTest.java
+++ src/test/java/com/saucelabs/tests/WorkflowTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -18,30 +19,30 @@ public class WorkflowTest {
     @Test
     public void workflowTest() {
 
-        DesiredCapabilities caps = DesiredCapabilities.chrome();
-        caps.setCapability("platform", "Windows 10");
-        caps.setCapability("version", "65.0");
-        caps.setCapability("seleniumVersion", "3.1.0");
+//        DesiredCapabilities caps = DesiredCapabilities.chrome();
+//        caps.setCapability("platform", "Windows 10");
+//        caps.setCapability("version", "65.0");
+//        caps.setCapability("seleniumVersion", "3.1.0");
+//
+//        String sauce_username = System.getenv("SAUCE_USERNAME");
+//        String sauce_access_key = System.getenv("SAUCE_ACCESS_KEY");
+//
+//        URL url = null;
+//        try {
+//            url = new URL("https://" + sauce_username + ":" + sauce_access_key + "@ondemand.saucelabs.com/wd/hub/");
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
 
-        String sauce_username = System.getenv("SAUCE_USERNAME");
-        String sauce_access_key = System.getenv("SAUCE_ACCESS_KEY");
+        driver = new ChromeDriver();
 
-        URL url = null;
-        try {
-            url = new URL("https://" + sauce_username + ":" + sauce_access_key + "@ondemand.saucelabs.com/wd/hub/");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        driver = new RemoteWebDriver(url, caps);
-
-        driver.get("https://www.saucedemo.com");
-
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.cssSelector(".btn_action")).click();
-
-        Assert.assertTrue(driver.getCurrentUrl().contains("/inventory.html"));
+//        driver.get("https://www.saucedemo.com");
+//
+//        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+//        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+//        driver.findElement(By.cssSelector(".btn_action")).click();
+//
+//        Assert.assertTrue(driver.getCurrentUrl().contains("/inventory.html"));
 
         driver.quit();
     }
